649352cfeef7be1526954b652c0de6a4ec553fd3
Konstantin Dimitrov <battlefield798@gmail.com>
2016-07-19 18:21:39 +0300

    Create PixelWatch_v1.0.ino

---
+++ PixelWatch_v1.0/PixelWatch_v1.0.ino
@@ -0,0 +1,55 @@
+#include <Wire.h>              // I2C aka TWI library
+#include <Time.h>              // Time manipulation librarry
+#include <DS1307RTC.h>         // RTC library
+#include <Adafruit_NeoPixel.h> // NeoPixel library
+
+#define PIN  5       // Which pin on the Arduino is connected to the NeoPixels?
+#define NUMPIXELS 12 // How many NeoPixels are attached to the Arduino?
+
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup()
+{
+  pixels.begin();          // This initializes the NeoPixel library.
+  pixels.setBrightness(25);// Sets brightness
+  Wire.begin();            // This initializes the Wire library.
+}
+
+void loop()
+{
+tmElements_t tm;
+      RTC.read(tm);
+      
+  uint8_t numHourPixels = tm.Hour;                // Time hours
+  uint8_t numMinPixels = ((tm.Minute) * 12) / 59; //Time minutes
+  uint8_t numSecondPixels = ((tm.Second)*12)/60;  // Time seconds
+
+  if (numSecondPixels == 0)
+    numSecondPixels = 12;
+  if (numHourPixels == 0)
+    numHourPixels = 12;
+  if (numHourPixels > 12)
+    numHourPixels -= 12;
+    if (numMinPixels == 0)
+    numMinPixels = 12;
+
+  uint8_t r, g, b;
+  for (int i=0; i<12; i++) {
+    r = 0;
+    g = 0;
+    b = 0;
+    if (i == numMinPixels - 1)
+      r = 255;
+    if (i == numSecondPixels - 1)
+      g = 255;
+    if (i == numHourPixels - 1)
+      b = 255;
+
+    pixel.setPixelColor(i, r, g, b); // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+  }
+  pixels.show(); // This sends the updated pixel color to the hardware.
+}
+
+
+
